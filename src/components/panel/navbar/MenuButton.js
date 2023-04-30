0d4ce0cb76a8759887ee3690e22de73a3e548611
Antix7 <84929498+Antix7@users.noreply.github.com>
2023-04-30 22:08:00 +0200

    Removed highlight when clicking buttons on mobile

--- src/components/panel/navbar/MenuButton.js
+++ src/components/panel/navbar/MenuButton.js
@@ -1,7 +1,8 @@
+import menuIcon from "../../../img/menu-icon.png"
 export default function MenuButton({ id, onClick }) {
   return (
     <button className="menuButton" id={id} onClick={onClick}>
-      <img src="menu-icon.png" alt="menu" height={30} width={30}/>
+      <img src={menuIcon} alt="menu" height={30} width={30}/>
     </button>
   )
 }
