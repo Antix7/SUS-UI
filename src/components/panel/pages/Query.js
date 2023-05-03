62b599e35c4779377489941137ca44804dbdae65
Antix7 <84929498+Antix7@users.noreply.github.com>
2023-05-03 13:51:10 +0200

    Implemented Query.js

--- src/components/panel/pages/Query.js
+++ src/components/panel/pages/Query.js
@@ -1,9 +1,50 @@
-import { useState } from 'react';
+import {useState} from "react";
+import axios from "axios";
+import authHeader from "../../../authHeader";
+import "../../css/contentPage.css"
 
 export default function Query() {
-  return (
-    <>
-      <p>Własne query</p>
-    </>
-  )
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [queryResult, setQueryResult] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setQueryResult(null);
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    axios.post(
+      `${process.env.REACT_APP_SERVER_DOMAIN}/query`,
+      formData,
+      {headers: authHeader()} // passing JWT
+    )
+      .then((response) => {
+        setErrorMessage(response.data.message);
+        if(response.data.success) setQueryResult(JSON.stringify(response.data.result));
+      }).catch((error) => {
+      setErrorMessage("Wystąpił błąd w komunikacji z serwerem")
+      console.log(error);
+    });
+  }
+
+  return (<div className="contentDiv">
+    <p className="contentTitle">Własne query do bazy danych</p>
+    <form
+      id="queryForm"
+      onSubmit={handleSubmit}
+    >
+      <textarea
+        className="textareaInput textInput"
+        name="query"
+        placeholder="Query"
+        rows="10"
+      ></textarea>
+
+      <button className="button" type="submit" id="submitButton">Wykonaj</button>
+    </form>
+    <p id="errorMessage">{errorMessage}</p>
+    <p id="queryResult">{queryResult}</p>
+  </div>)
 }
