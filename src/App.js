97ca233f193be7cd7c693292776095ae0fb0aa72
Antix7 <84929498+Antix7@users.noreply.github.com>
2023-05-05 14:48:37 +0200

    Users stay logged in on page reload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import AdminPanel from "./components/panel/AdminPanel";
 import LoginPanel from "./components/login/LoginPanel";
 import axios from "axios";
+import authHeader from "./authHeader";
 
 export default function App() {
 
@@ -10,12 +11,23 @@ export default function App() {
 
   function handleLogin(username, token) {
     setUsername(username);
+    sessionStorage.setItem("username", username);
     sessionStorage.setItem("token", token);
   }
   function handleLogout() {
     setUsername(null);
+    sessionStorage.removeItem("username");
     sessionStorage.removeItem("token");
   }
+
+  useEffect(() => {
+    axios.post(`${process.env.REACT_APP_SERVER_DOMAIN}/auth`,
+      null,
+      {headers: authHeader()})
+      .then((response) => {
+        if(response.data.success) setUsername(sessionStorage.getItem("username"));
+      });
+  }, []);
 
   return (
     <>
